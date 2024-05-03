484e002d435e2d3f6300ba652719adca57841601
CharlesLopezE <150065016+CharlesLopezE@users.noreply.github.com>
2024-05-02 19:57:05 -0700

    The mage is complete
    completed the mage

--- GameProject.java
+++ GameProject.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GameProject {
     public static void main(String[] args) {
@@ -30,12 +31,12 @@ public class GameProject {
 
     // Placeholder for character selection
     private static void playGame() {
-        // // Introduce a 3-second delay
-        // try {
-        //     Thread.sleep(2000); // 2000 milliseconds = 2 seconds
-        // } catch (InterruptedException e) {
-        //     e.printStackTrace();
-        // }
+        // Introduce a 3-second delay
+        try {
+            Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // Continue with the game...
         System.out.println();
@@ -52,6 +53,7 @@ public class GameProject {
         }
         // allows to input the chracter names that is above inside the string...
         Scanner gameInput = new Scanner (System.in);
+        Random random = new Random();
 
         boolean validHero = false;
 
@@ -65,11 +67,11 @@ public class GameProject {
         System.out.println("Welcome " + heroName + "! Let your journey commence!");
         System.out.println();
 
-        // try {
-        //     Thread.sleep(2000); // 2000 milliseconds = 2 seconds
-        // } catch (InterruptedException e) {
-        //     e.printStackTrace();
-        // }
+        try {
+            Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         switch (heroName){
             case "Knight":
                 System.out.println( "Your quest is to save the princess that is held captive in a castle!");
@@ -98,11 +100,11 @@ public class GameProject {
                       System.out.println("Your health: " + health);
                       System.out.println();
                 }
-                // try {
-                //     Thread.sleep(2000); // 2000 milliseconds = 2 seconds
-                // } catch (InterruptedException e) {
-                //     e.printStackTrace();
-                // }
+                try {
+                    Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("You come across a town and a townfolk aproaches you.");
                 System.out.println();
                 System.out.println("Townfoll: 'You there! are you a mage!?'");
@@ -118,6 +120,12 @@ public class GameProject {
                     userResp = gameInput.nextLine();
                 }
 
+                try {
+                    Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
                 if (userResp.equalsIgnoreCase("yes")){
                     System.out.println();
                     System.out.println("Townfolk: 'Ah, you are! Then you are well enough to take down the corrupted wizard!'");
@@ -130,8 +138,16 @@ public class GameProject {
 
                     String chestUse = gameInput.nextLine();
 
+                    try {
+                        Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
                     if (chestUse.equalsIgnoreCase("yes")){
+                        System.out.println();
                         System.out.println("You opened the chest and found the following items:");
+                        System.out.println();
                         System.out.println("1. Scroll of Lightning...");
                         System.out.println("2. Health Potion...");
                         System.out.println("3. Metal Shield...");
@@ -168,8 +184,98 @@ public class GameProject {
                 System.out.println();
                 System.out.println("You have reached the entrance of the cathedral.");
                 System.out.println();
+                try {
+                    Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("You decided to enter to face the wizard.");
                 System.out.println();
+                try {
+                    Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("The air crackles with magic, and at the altar stands the corrupted wizard...");
+                System.out.println();
+                try {
+                    Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("He slowly turns to face you, eyes glowing with malevolence...");
+                System.out.println();
+                try {
+                    Thread.sleep(2000); // 2000 milliseconds = 2 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("The battle begins immediately!");
+                System.out.println();
+
+                int playerHP = 80;
+                int wizardHP = 100;
+
+                while (playerHP > 0 && wizardHP > 0) {
+                    System.out.println("Your Health: " + playerHP);
+                    System.out.println("Wizard Health: " + wizardHP);
+                    System.out.println();
+                    System.out.println("Choose your action:");
+                    System.out.println("1. Attack");
+                    System.out.println("2. Cast spell");
+                    int playerAct = gameInput.nextInt();
+                    gameInput.nextLine();
+
+                    switch (playerAct){
+                        case 1:
+                            if(random.nextDouble() < 0.8){
+                                int playerDam = 20;
+                                wizardHP -= playerDam;
+                                System.out.println();
+                                System.out.println("You dealt damage to the wizard with your staff! Wizard's health -" + playerDam);
+                                System.out.println();
+                            } else {
+                                System.out.println();
+                                System.out.println("Your attack missed!");
+                                System.out.println();
+                            }
+                            break;
+                        case 2:
+                        if(random.nextDouble() < 0.7){
+                            int spellDam = 30;
+                            wizardHP -= spellDam;
+                            System.out.println();
+                            System.out.println("You striked the wizard with your spell! Wizard's health -" + spellDam);
+                        } else {
+                            System.out.println("Your attack misses!");
+                            System.out.println();
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid action. You hesistated...");
+                        System.out.println();
+                    }
+
+                    if (random.nextDouble() < 0.7){
+                        int wizardDam = 25;
+                        playerHP -= wizardDam;
+                        System.out.println();
+                        System.out.println("The wizard retaliates with dark magic! Your health-" + wizardDam);
+                        System.out.println();
+                    } else {
+                        System.out.println("The wizard's attack misses!");
+                        System.out.println();
+                    }
+                }
+
+                if (playerHP <= 0) {
+                    System.out.println("You have been defeated. The corrupted wizard triumphs the town.");
+                    System.out.println();
+                } else {
+                    System.out.println("Congrats! You have defeated the corrupted wizard!");
+                    System.out.println();
+                    System.out.println("You emerge victorius and restore peace to the land.");
+                }
             break;
             case "Assassin":
                 System.out.println("You will find the ancient artifact to save the creed!");
